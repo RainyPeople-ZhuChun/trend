dd5fdd13e5a65db90dbadb6ec6339ab6f82ba048
zhuchun <zhuchun@rainypeople.com>
2020-12-05 09:38:10 +0800

    完成收益对比图

--- trend-trading-backtest-service/src/main/java/com/rainypeople/trend/service/BackTestService.java
+++ trend-trading-backtest-service/src/main/java/com/rainypeople/trend/service/BackTestService.java
@@ -2,11 +2,11 @@ package com.rainypeople.trend.service;
 
 import com.rainypeople.trend.client.IndexDataClient;
 import com.rainypeople.trend.pojo.IndexData;
+import com.rainypeople.trend.pojo.Profit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BackTestService {
@@ -18,10 +18,136 @@ public class BackTestService {
         List<IndexData> result = indexDataClient.getIndexData(code);
         Collections.reverse(result);
 
-        for (IndexData indexData:result){
+        /*for (IndexData indexData:result){
             System.out.println(indexData.getDate());
-        }
+        }*/
 
         return result;
+    }
+
+    public Map<String,Object> simulate(int ma, float sellSate, float buySate, float serviceCharge, List<IndexData> allIndexDatas) {
+        List<Profit> profits=new ArrayList<>();
+        //初始资金
+        float initCash= 1000;
+        //抛出获得的金额(在买入卖出后的资金)
+        float cash= initCash;
+        //份额
+        float share=0;
+        //购买的股票现在的总价值
+        float value=0;
+
+        //初始值每股价格
+        float init=0;
+        if (!allIndexDatas.isEmpty()){
+            init=allIndexDatas.get(0).getClosePoint();
+        }
+        for (int i=0;i<allIndexDatas.size();i++){
+            IndexData indexData = allIndexDatas.get(i);
+            float closePoint = indexData.getClosePoint();
+            //取得平均值
+            float avg=getMA(i,ma,allIndexDatas);
+            //取得最高点值
+            float max=getMax(i,ma,allIndexDatas);
+            //上升率
+            float increase_sate;
+            //回跌率
+            float decrease_sate;
+
+            if (0==avg){
+                increase_sate=0;
+            }
+            increase_sate=closePoint/avg;
+            if (0==max){
+                decrease_sate=0;
+            }
+            decrease_sate=closePoint/max;
+
+            if (0!=avg){
+                //如果上涨率超过购买阈值，买入最大量
+                if (increase_sate>buySate){
+                    if (0==share){
+                        share=cash/closePoint;
+                        cash=0;
+                    }
+                //如果下跌率低于了抛出阈值，全部抛出
+                }else if(decrease_sate<sellSate){
+                    if (0!=share){
+                        cash=share*closePoint*(1-serviceCharge);
+                        share=0;
+                    }
+                //如果在两者中间，什么都不做
+                }else {
+
+                }
+            }
+
+            if (0!=share){
+                value=share*closePoint;
+            }else {
+                value=cash;
+            }
+
+            //回报率
+            float rate=value/initCash;
+
+            Profit profit=new Profit();
+            profit.setDate(indexData.getDate());
+            profit.setValue(rate*init);
+
+            System.out.println("profit.value="+profit.getValue());
+            profits.add(profit);
+        }
+
+        Map<String,Object> result=new HashMap<>();
+        result.put("profits",profits);
+        return result;
+    }
+
+    public float getMax(int i, int ma, List<IndexData> allIndexDatas) {
+        //moving average移动均线的天数开始日期，比如ma=20，那么前20天都不会有值，知道21从0开始
+        int start=i-1-ma;
+        if (start<0){
+            start=0;
+        }
+        //到前一天
+        int now=i-1;
+
+        if (start<0){
+            return 0;
+        }
+
+        float max=0;
+        for (int j=start;j<now;j++){
+            IndexData indexData = allIndexDatas.get(j);
+            float closePoint = indexData.getClosePoint();
+            if (closePoint>max){
+                max=closePoint;
+            }
+        }
+        return max;
+    }
+
+    public float getMA(int i, int ma, List<IndexData> allIndexDatas) {
+        //moving average移动均线的天数开始日期，比如ma=20，那么前20天都不会有值，知道21从0开始
+        int start=i-1-ma;
+        //到前一天
+        int now=i-1;
+
+        if (start<0){
+            return 0;
+        }
+
+        float sum=0;
+        float avg=0;
+        for (int j=start;j<now;j++){
+            IndexData indexData = allIndexDatas.get(j);
+            sum += indexData.getClosePoint();
+        }
+        int dateCount=now-start;
+        if (0==dateCount){
+            avg=0;
+        }
+        avg=sum/dateCount;
+        return avg;
     }
 }
--- trend-trading-backtest-service/src/main/java/com/rainypeople/trend/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/com/rainypeople/trend/web/BackTestController.java
@@ -1,6 +1,10 @@
 package com.rainypeople.trend.web;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.rainypeople.trend.pojo.IndexData;
+import com.rainypeople.trend.pojo.Profit;
 import com.rainypeople.trend.service.BackTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,9 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class BackTestController {
@@ -18,12 +20,58 @@ public class BackTestController {
     @Autowired
     BackTestService backTestService;
 
-    @GetMapping("/simulate/{code}")
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String,Object> backtest(@PathVariable("code")String code)throws Exception{
+    public Map<String,Object> backtest(@PathVariable("code")String code,
+                                       @PathVariable("startDate")String strStartDate,
+                                       @PathVariable("endDate")String strEndDate)throws Exception{
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
+
+        String indexStartDate = allIndexDatas.get(0).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size() - 1).getDate();
+
+        //过滤日期，只需要大于等于开始日期，小于等于结束日期的数据
+        allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
+
+        //moving average移动均线,20天的
+        int ma=20;
+        //抛出阈值
+        float sellSate=0.95f;
+        //购买阈值
+        float buySate=1.05f;
+        //手续费
+        float serviceCharge=0f;
+
+        //模拟服务
+        Map<String,?> simulateResult=backTestService.simulate(ma,sellSate,buySate,serviceCharge,allIndexDatas);
+        //从map集合中取出所有的利润类profits
+        List<Profit> profits= (List<Profit>) simulateResult.get("profits");
+
         Map<String,Object> result=new HashMap<>();
         result.put("indexDatas",allIndexDatas);
+        result.put("indexStartDate",indexStartDate);
+        result.put("indexEndDate",indexEndDate);
+        result.put("profits",profits);
         return result;
     }
+
+    private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
+        if (StrUtil.isBlankOrUndefined(strStartDate)&&StrUtil.isBlankOrUndefined(strEndDate)){
+            return allIndexDatas;
+        }
+        List<IndexData> result=new ArrayList<>();
+        Date startDate = DateUtil.parse(strStartDate);
+        Date endDate = DateUtil.parse(strEndDate);
+
+        for (IndexData indexData:allIndexDatas){
+            Date date=DateUtil.parse(indexData.getDate());
+            if (date.getTime()>=startDate.getTime()&&date.getTime()<=endDate.getTime()){
+                result.add(indexData);
+            }
+        }
+
+        return result;
+    }
+
+
 }
